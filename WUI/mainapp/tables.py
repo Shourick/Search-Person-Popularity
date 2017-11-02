7d5f3a8a53b0fd13448c81b0502bd6eef7498c7e
AlexanderKorobyin <alexanderkorobyin@gmail.com>
2017-11-02 13:49:51 +0300

    + Добавил tables.py в mainapp для создания кастомных таблиц
    + Вывел простую таблицу на странице общей статистики (reverted from commit 7e2ea6406d4de2e43b245f8434b871ffcf9e0d6e)

--- WUI/mainapp/tables.py
+++
@@ -1,6 +0,0 @@
-import django_tables2 as tables
-from .models import Politic
-
-class GeneralStatisticsTable(tables.Table):
-    class Meta:
-        model = Politic
--- WUI/mainapp/views.py
+++ WUI/mainapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from mainapp.models import *
-from .tables import GeneralStatisticsTable
 from django.views.generic.list import ListView
 # Create your views here.
 
@@ -12,8 +11,7 @@ def general(request):
     title = 'Общая статистика'
     sites = Site.objects.all()
     politics = Politic.objects.all()
-    gs_table = GeneralStatisticsTable(politics)
-    return render(request,'general.html', {'title':title, 'sites': sites, 'politics': politics, 'gs_table': gs_table})
+    return render(request,'general.html', {'title':title, 'sites': sites, 'politics': politics})
 
 def daily(request):
     title = 'Ежедневная статистика'
