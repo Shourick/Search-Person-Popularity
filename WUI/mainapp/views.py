bf457054c2e979ef2bd23338e2c4a9c375fc10ba
viva-fidz <viva.fidz@yandex.ru>
2017-11-06 00:49:47 +0300

    PEP8

--- WUI/mainapp/views.py
+++ WUI/mainapp/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from mainapp.models import *
-from django.views.generic.list import ListView
-# Create your views here.
+
+
+# TODO: адекватный listView
+
 
 def index(request):
     title = 'Главная'
-    return render(request,'index.html', {'title':title})
+    return render(request, 'index.html', {'title': title})
+
 
 def general(request):
     title = 'Общая статистика'
     sites = Site.objects.all()
     politics = Politic.objects.all()
-    return render(request,'general.html', {'title':title, 'sites': sites, 'politics': politics})
+    return render(request, 'general.html', {'title': title, 'sites': sites, 'politics': politics})
+
 
 def daily(request):
     title = 'Ежедневная статистика'
     sites = Site.objects.all()
     politics = Politic.objects.all()
-    return render(request,'daily.html', {'title':title, 'sites': sites, 'politics': politics})
-
+    return render(request, 'daily.html', {'title': title, 'sites': sites, 'politics': politics})
