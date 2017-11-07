7676b7edcc8af07e07e1bd3c0e9ca1ea6178ff4e
AlexanderKorobyin <alexanderkorobyin@gmail.com>
2017-11-07 12:51:04 +0300

    Исправления в fill_db.py

--- WUI/mainapp/management/commands/fill_db.py
+++ WUI/mainapp/management/commands/fill_db.py
@@ -1,33 +1,18 @@
 from django.core.management.base import BaseCommand
 from mainapp.models import *
+from django.utils import timezone
+from datetime import datetime
 
 
 class Command(BaseCommand):
     help = 'Fill DB fake data'
 
     def handle(self, *args, **options):
-        keywords = [
-            {'id': '1', 'name': 'Путин', 'person_id': 1},
-            {'id': '2', 'name': 'Путина', 'person_id': 1},
-            {'id': '3', 'name': 'Путину', 'person_id': 1},
-        ]
 
         politics = [
             {'id': '1', 'name': 'Путин'},
             {'id': '2', 'name': 'Медведев'},
             {'id': '3', 'name': 'Навальный'},
-        ]
-
-        person_page_ranks = [
-            {'person_id': '1', 'page_id': '1', 'rank': 3},
-            {'person_id': '2', 'page_id': '1', 'rank': 3},
-            {'person_id': '3', 'page_id': '1', 'rank': 3},
-        ]
-
-        pages = [
-            {'id': '1', 'url': 'Lenta.ru', 'site_id': '1', 'found_date_time': '10-10-2017 15:23', 'last_scan_date': '11-10-2017 15:23'},
-            {'id': '2', 'url': 'rbc.ru', 'site_id': '2', 'found_date_time': '10-10-2017 15:23', 'last_scan_date': '11-10-2017 15:23'},
-            {'id': '2', 'url': 'bbc.ru', 'site_id': '2', 'found_date_time': '10-10-2017 15:23', 'last_scan_date': '11-10-2017 15:23'},
         ]
 
         sites = [
@@ -36,28 +21,51 @@ class Command(BaseCommand):
             {'id': '3', 'name': 'bbc.ru'},
         ]
 
-        Keywords.objects.all().delete()
-        for keyword in keywords:
-            keyword = Keywords(**keyword)
-            keyword.save()
+        keywords = [
+            {'id': '1', 'name': 'Путин', 'person_id': Politic.objects.get(pk=1)},
+            {'id': '2', 'name': 'Путина', 'person_id': Politic.objects.get(pk=1)},
+            {'id': '3', 'name': 'Путину', 'person_id': Politic.objects.get(pk=1)},
+        ]
+
+        time_format = "%Y-%m-%d %H:%M"
+        found_date_time = timezone.make_aware(datetime.strptime('2017-10-10 15:23', time_format))
+        last_scan_date = timezone.make_aware(datetime.strptime('2017-11-10 15:23', time_format))
+
+        pages = [
+            {'id': '1', 'url': 'Lenta.ru', 'site_id': Site.objects.get(pk=1), 'found_date_time': found_date_time, 'last_scan_date': last_scan_date},
+            {'id': '2', 'url': 'rbc.ru', 'site_id': Site.objects.get(pk=2), 'found_date_time': found_date_time, 'last_scan_date': last_scan_date},
+            {'id': '3', 'url': 'bbc.ru', 'site_id': Site.objects.get(pk=3), 'found_date_time': found_date_time, 'last_scan_date': last_scan_date},
+        ]
+
+        person_page_ranks = [
+            {'person_id': Politic.objects.get(pk=1), 'page_id': Pages.objects.get(pk=1), 'rank': 3},
+            {'person_id': Politic.objects.get(pk=2), 'page_id': Pages.objects.get(pk=1), 'rank': 3},
+            {'person_id': Politic.objects.get(pk=3), 'page_id': Pages.objects.get(pk=1), 'rank': 3},
+        ]
 
         Politic.objects.all().delete()
         for politic in politics:
             politic = Politic(**politic)
             politic.save()
 
-        PersonPageRank.objects.all().delete()
-        for person_page_rank in person_page_ranks:
-            person_page_rank = PersonPageRank(**person_page_rank)
-            person_page_rank.save()
+        Site.objects.all().delete()
+        for site in sites:
+            site = Site(**site)
+            site.save()
+
+        Keywords.objects.all().delete()
+        for keyword in keywords:
+            keyword = Keywords(**keyword)
+            keyword.save()
 
         Pages.objects.all().delete()
         for page in pages:
             page = Pages(**page)
             page.save()
 
-        Site.objects.all().delete()
-        for site in sites:
-            site = Site(**site)
-            site.save()
+        PersonPageRank.objects.all().delete()
+        for person_page_rank in person_page_ranks:
+            person_page_rank = PersonPageRank(**person_page_rank)
+            person_page_rank.save()
+
 
