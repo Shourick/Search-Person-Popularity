04e399e2585217ed202678c9f2d2346cfe3b6ad6
viva-fidz <viva.fidz@yandex.ru>
2017-11-08 01:56:16 +0300

    query для рендера

--- WUI/mainapp/views.py
+++ WUI/mainapp/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
-from mainapp.models import *
+from .models import *
 from .tables import GeneralStatisticsTable, DailyStatisticsTable
-from django.views.generic.list import ListView
-# Create your views here.
+
 
 def index(request):
     title = 'Главная'
-    return render(request,'index.html', {'title':title})
+    return render(request, 'index.html', {'title': title})
+
 
 def general(request):
     title = 'Общая статистика'
     sites = Site.objects.all()
+    politics = Politic.objects.all()
     person_page_rank = PersonPageRank.objects.all()
     gs_table = GeneralStatisticsTable(person_page_rank)
-    return render(request,'general.html', {'title':title, 'sites': sites,'gs_table': gs_table})
+    return render(request, 'general.html', {'title': title, 'sites': sites, 'politics': politics, 'gs_table': gs_table})
+
 
 def daily(request):
     title = 'Ежедневная статистика'
     sites = Site.objects.all()
     politics = Politic.objects.all()
     ds_table = DailyStatisticsTable(politics)
-    return render(request,'daily.html', {'title':title, 'sites': sites, 'politics': politics, 'ds_table': ds_table})
-
+    return render(request, 'daily.html', {'title': title, 'sites': sites, 'politics': politics, 'ds_table': ds_table})
