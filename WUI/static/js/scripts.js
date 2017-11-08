0d1799c0c30e0f241ff81561fbe9fc0461d175a6
viva-fidz <viva.fidz@yandex.ru>
2017-11-08 04:59:18 +0300

    получаем данные от юзера (надо посмотреть что с site.id в general_statistic)

--- WUI/static/js/scripts.js
+++ WUI/static/js/scripts.js
@@ -1,5 +1,21 @@
-$('.selectpicker').selectpicker({
-  style: 'btn-info',
-  size: 4
+$(document).ready(function(){
+    var form = $('#general_stat');
+    form.on('submit', function(e){
+        e.preventDefault();
+        console.log('ok');
+        var submit_btn = $('#submit_btn');
+        var site_id = submit_btn.data('site_id');
+        console.log('site_id=' + site_id);
+    })
 });
+//
+//$(document).ready(function(){
+//    var form = $('#"daily_stat');
+//    form.on('submit', function(e){
+//        e.preventDefault();
+//        var submit_btn = $('#submit_btn');
+//        var date_from = $('#date_from').val();
+//        var date_to = $('#date_to').val();
 
+//    })
+//});
