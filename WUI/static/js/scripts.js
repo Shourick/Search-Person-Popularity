e292f51dd1f4a481e180115333fbb4049ce147f8
viva-fidz <viva.fidz@yandex.ru>
2017-11-13 22:29:06 +0300

    remove hidden class

--- WUI/static/js/scripts.js
+++ WUI/static/js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-    var form = $('#general_stat');
+    var form = $('.stat');
     form.on('submit', function(e){
         e.preventDefault();
         console.log('ok');
-//        var submit_btn = $('#submit_btn');
         var site_id = $('#id').val();
         console.log('site_id=' + site_id);
+        $('#politics-table').removeClass('hidden');
     })
 });
+
 //
 //$(document).ready(function(){
 //    var form = $('#"daily_stat');
