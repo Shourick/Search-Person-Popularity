53197838c86cf0c6b61fd7df36a9d9d30f4d267f
Hteew <viva.fidz@yandex.ru>
2017-11-29 07:55:01 +0300

    API_HOST вынесен в settings

--- WUI/mainapp/views.py
+++ WUI/mainapp/views.py
@@ -5,19 +5,20 @@ from django.contrib.auth.decorators import login_required
 from requests.auth import HTTPBasicAuth
 import requests
 from .forms import ContactSupportForm
+from spp import settings
 
 
 def index(request):
     title = 'Главная'
-    sites = requests.get("http://94.130.27.143/sites", auth=HTTPBasicAuth('root', 'root_password')).json()
-    persons = requests.get("http://94.130.27.143/persons", auth=HTTPBasicAuth('root', 'root_password')).json()
+    sites = requests.get(settings.API_HOST + "sites/", auth=HTTPBasicAuth('root', 'root_password')).json()
+    persons = requests.get(settings.API_HOST + "persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
     return render(request, 'index.html', {'title': title, 'sites': sites, 'persons': persons})
 
 
 def keywords(request):
     title = 'Ключевые слова'
-    persons = requests.get("http://94.130.27.143/persons", auth=HTTPBasicAuth('root', 'root_password')).json()
-    keywords = requests.get("http://94.130.27.143/keywords", auth=HTTPBasicAuth('root', 'root_password')).json()
+    persons = requests.get(settings.API_HOST + "persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
+    keywords = requests.get(settings.API_HOST + "keywords/", auth=HTTPBasicAuth('root', 'root_password')).json()
     return render(request, 'keywords.html', {'title': title, 'persons': persons, 'keywords': keywords})
 
 
@@ -41,20 +42,19 @@ def support(request):
             return render(request, 'email/thanks.html', {'title': 'Спасибо'})
     else:
         form = ContactSupportForm()
-    return render(request, 'email/support.html', {'title': title, 'form': form})
+        return render(request, 'email/support.html', {'title': title, 'form': form})
 
 
-@login_required
 def general(request):
     title = 'Общая статистика'
     if request.method == 'POST':
         person_id = request.POST.get('person_id')
-        url = "http://94.130.27.143/rank/" + str(person_id)
+        url = f"{settings.API_HOST}rank/{person_id}"
         ranks = requests.get(url, auth=HTTPBasicAuth('root', 'root_password')).json()
-        sites = requests.get("http://94.130.27.143/sites/", auth=HTTPBasicAuth('root', 'root_password')).json()
+        sites = requests.get(settings.API_HOST + "sites/", auth=HTTPBasicAuth('root', 'root_password')).json()
         return render(request, 'rank.html', {'title': title, 'ranks': ranks, 'sites': sites})
     else:
-        persons = requests.get("http://94.130.27.143/persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
+        persons = requests.get(settings.API_HOST + "persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
         return render(request, 'general.html', {'title': title, 'persons': persons})
 
 
@@ -65,15 +65,14 @@ def rank(request):
 @login_required
 def daily(request):
     title = 'Ежедневная статистика'
-    persons = requests.get("http://94.130.27.143/persons", auth=HTTPBasicAuth('root', 'root_password')).json()
-    sites = requests.get("http://94.130.27.143/sites", auth=HTTPBasicAuth('root', 'root_password')).json()
+    persons = requests.get(settings.API_HOST + "persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
+    sites = requests.get(settings.API_HOST +"sites/", auth=HTTPBasicAuth('root', 'root_password')).json()
     if request.method == 'POST':
         person_id = request.POST.get('person_id')
         site_id = request.POST.get('site_id')
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
-        url = "http://94.130.27.143/rank/" + str(person_id) + '/' + str(site_id) + '/' + \
-              str(start_date) + '&' + str(end_date)
+        url = f"{settings.API_HOST}rank/{person_id}/{site_id}/{start_date}&{end_date}"
         rank = requests.get(url, auth=HTTPBasicAuth('root', 'root_password')).json()
         return render(request, 'rank_daily.html', {'title': title, 'ranks': rank})
     else:
